9bfb40f84569d68a91259ca6febeaba65724c55e
LucasArtur <lucasarturbh@hotmail.com>
2021-05-14 09:14:17 -0300

    Project created

---
+++ src/main/java/com/lucasartur/course/entities/User.java
@@ -0,0 +1,80 @@
+package com.lucasartur.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+	private String nome;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User () {
+	}
+
+	public User(Long id, String nome, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(Long id) {
+		super();
+		this.id = id;
+	}
+	
+	
+}
